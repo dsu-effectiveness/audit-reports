5305999cd3d1fd487475aed37f0864dd2eb466d3
Justin Pfeifer <Justin.Pfeifer@dixie.edu>
2021-07-15 13:57:49 -0600

    Added scbcrse to data pull

--- sql/courses.sql
+++ sql/courses.sql
@@ -1,9 +1,11 @@
-         SELECT ssbsect_term_code AS term,
+         SELECT DISTINCT ssbsect_term_code AS term,
                 ssbsect_crn AS crn,
                 ssbsect_subj_code AS subject_code,
                 ssbsect_crse_numb AS course_number,
                 ssbsect_seq_numb AS section_number,
                 ssbsect_insm_code AS instruction_method,
+                ssbsect_schd_code AS schedule_code,
+                stvschd_desc AS schedule_desc,
                 ssbsect_camp_code AS campus_code,
                 ssbsect_enrl AS enrollment,
                 ssbsect_ssts_code AS active_ind,
@@ -18,8 +20,11 @@
                 room_code2 AS room_code_4,
                 bldg_code2 AS building_code_5,
                 room_code2 AS room_code_5,
-                begin_time1 AS start_time_1
- --               d.scbsupp_occs_code AS occs_code
+                begin_time1 AS start_time_1,
+                scbcrse_credit_hr_low AS credit_hours,
+                scbcrse_lec_hr_low AS lecture_hours,
+                scbcrse_lab_hr_low AS lab_hours,
+                scbcrse_oth_hr_low AS other_hours
            FROM as_catalog_schedule a
      INNER JOIN ssbsect b
              ON b.ssbsect_crn = a.crn_key
@@ -27,25 +32,17 @@
      INNER JOIN ssrsccd c
              ON c.ssrsccd_crn = b.ssbsect_crn
             AND c.ssrsccd_term_code = a.term_code_key
---       LEFT JOIN (SELECT *
---                 FROM scbsupp a1
---           INNER JOIN voccrs_current b1 ON b1.subj = a1.scbsupp_subj_code
---                  AND b1.crse = a1.scbsupp_crse_numb
---                  AND scbsupp_eff_term  =
---                     (SELECT MAX(b.scbsupp_eff_term)
---                        FROM scbsupp b, scbcrky c
---                       WHERE b.scbsupp_subj_code = scbcrky_subj_code
---                         AND b.scbsupp_crse_numb = scbcrky_crse_numb
---                         AND b.scbsupp_subj_code = a.scbcrse_subj_code
---                         AND b.scbsupp_crse_numb = a.scbcrse_crse_numb
---                         AND c.scbcrky_term_code_end = '999999'
---                    GROUP BY b.scbsupp_subj_code,
---                             b.scbsupp_crse_numb
---                     )
---                  ) d ON d.scbsupp_crse_numb = b.ssbsect_crse_numb
---            AND scbsupp_eff_term = b.ssbsect_term_code
-          WHERE ssts_code = 'A'
-            AND camp_code <> 'XXX'
+     LEFT JOIN scbcrse d
+            ON d.scbcrse_subj_code = b.ssbsect_subj_code
+           AND d.scbcrse_crse_numb = b.ssbsect_crse_numb
+           AND d.scbcrse_eff_term = (SELECT MAX(scbcrse_eff_term)
+                                       FROM scbcrse d1
+                                      WHERE d1.scbcrse_subj_code = d.scbcrse_subj_code
+                                        AND d1.scbcrse_crse_numb = d.scbcrse_crse_numb)
+     LEFT JOIN stvschd e
+            ON e.stvschd_code = b.ssbsect_schd_code
+          WHERE a.ssts_code = 'A'
+            AND camp_code != 'XXX'
             AND (ssbsect_term_code = (SELECT dsc.f_get_term(SYSDATE, 'nterm') - 10 AS prior_term FROM dual)
              OR ssbsect_term_code = (SELECT dsc.f_get_term(SYSDATE, 'nterm') AS current_term FROM dual)
              OR ssbsect_term_code = (SELECT dsc.f_get_term(SYSDATE, 'nterm') + 10 AS future_term FROM dual));
